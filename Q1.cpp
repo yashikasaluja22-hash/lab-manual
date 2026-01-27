067b307ec19f81e575219f38829b8eaa441613c4
yashikasaluja22-hash <yashikasaluja22@gmail.com>
2026-01-27 13:58:03 +0530

    quesdone

--- Q1.cpp
+++ Q1.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+int main() {
+double mark1, mark2, mark3;
+cout << "Enter marks for student 1: ";
+cin >> mark1;
+cout << "Enter marks for student 2: ";
+cin >> mark2;
+cout << "Enter marks for student 3: ";
+cin >> mark3;
+double Total = (mark1 + mark2 + mark3)/(double)3;
+cout << "Average is: " << Total;
+return 0;
+}
