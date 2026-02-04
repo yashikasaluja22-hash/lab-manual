16cdc23639fde1a7a80415bd97e5233b69dd36ba
yashikasaluja22-hash <yashikasaluja22@gmail.com>
2026-02-04 09:46:31 +0530

    hdujw

--- Q4.cpp
+++ Q4.cpp
@@ -15,7 +15,6 @@ cout << "Enter unit price of one item: ";
 cin >> price;
 
 double amount = 0;
-cout << "Amount after 20% discount is: \n";
 amount += (price * quantity) * ((double)20/100);
 cout << '$' << amount;
 return 0;
